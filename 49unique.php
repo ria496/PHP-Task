9e1d505055bdbee7a536d43efce20feecf3002fe
ria496 <56160500+ria496@users.noreply.github.com>
2020-11-25 19:41:48 +0530

    Update 49unique.php

--- 49unique.php
+++ 49unique.php
@@ -1,29 +1,4 @@
 <?php
-
-// $num = array(1,2,3,3,4,4,5,6,6,7);
-// $cout = count($num);
-// for($i=0; i<$cout; $i++){
-//         echo $num[$i];
-//     }
-    
-//   for($i=0; $i<$cout; $i++){
-//         $c=1;
-//         if($num[$i]!=-1){
-// 		    for($j=$i+1; $j<$cout; $j++){
-//         	   if($num[$i]==$num[$j]){
-// 			       $c++;
-// 			       $num[$j]=-1;
-// 		       }
-// 	       }
-           
-//             $b[$i]=$c;
-//            echo $b[$i];
-// 		}
-
-        
-
-
-
   $num = array(1,2,3,3,4,4,5,5,6,6,7);
   $cout = count($num);
   for($i=0;$i<$cout;$i++){
@@ -36,12 +11,7 @@
         
        // if ($i == $j)
         }
-
-    
-      
-    
-          
-     }
+      }
      if($i==$j){
          echo $num[$i];
      }
